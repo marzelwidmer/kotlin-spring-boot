c484dc4b7fe1b958a217ef7b7409ff2dddd9883d
Marcel Widmer <marzelwidmer@gmail.com>
2018-03-30 10:11:30 +0200

    kotlin String

---
+++ src/test/kotlin/ch/keepcalm/kotlin/MyKtString.kt
@@ -0,0 +1,36 @@
+package ch.keepcalm.kotlin
+
+fun main(args:Array<String>){
+
+    var aString = "Hello!"
+    println(aString)
+
+    val emptyString = String()
+    println(emptyString)
+
+    val charArray = aString.toCharArray()
+    println(charArray.toList())
+
+    val byteArray = aString.toByteArray()
+    println(byteArray.toList())
+
+
+    aString += " and Welcome!"
+    println(aString)
+
+    val len  =  aString.length
+    for (c in aString) {
+        println(c)
+    }
+
+    val p = aString.indexOf("W")
+    val sub = aString.substring(p)
+    println(sub)
+
+    val string2 = aString.toUpperCase()
+    val match = aString.equals(string2)
+    println("Do they match $match")
+
+    println("Do they match with ignore uppercase ${aString.equals(string2, true)}")
+
+}
