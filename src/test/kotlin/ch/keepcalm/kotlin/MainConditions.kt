43be36ffd8dc313e88eb9464407a53eb19eaee85
Marcel Widmer <marzelwidmer@gmail.com>
2018-03-30 13:13:31 +0200

    if else -> switch

--- src/test/kotlin/ch/keepcalm/kotlin/MainConditions.kt
+++ src/test/kotlin/ch/keepcalm/kotlin/MainConditions.kt
@@ -4,7 +4,15 @@ fun main(args: Array<String>) {
     println("Enter a state abbreviation: ")
     val state = readLine()
 
-    if (state == "CA")println("The capital is Sacremento")
-    else if (state == "OR") println("The capital is Salem")
-    else println("I don`t know that sate")
+//    if (state == "CA")println("The capital is Sacremento")
+//    else if (state == "OR") println("The capital is Salem")
+//    else println("I don`t know that sate")
+
+    val capital: String?
+    when (state) {
+        "CA" -> capital = "Sacramento"
+        "OR" -> capital = "Salem"
+        else -> capital = "Unknown"
+    }
+    println("The capital is $capital")
 }
