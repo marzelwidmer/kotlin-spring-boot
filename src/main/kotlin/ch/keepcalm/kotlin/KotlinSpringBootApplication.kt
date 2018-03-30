742f2f16a86d596c2a27c33db38aa17ddeab21c3
Marcel Widmer <marzelwidmer@gmail.com>
2018-03-30 07:28:11 +0200

    init commit

---
+++ src/main/kotlin/ch/keepcalm/kotlin/KotlinSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package ch.keepcalm.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringBootApplication>(*args)
+}
