f36c85a9c6b31f92bf5aca7597bab15992413548
Marcel Widmer <marzelwidmer@gmail.com>
2018-03-30 12:39:16 +0200

    constants

---
+++ src/test/kotlin/ch/keepcalm/kotlin/Constants.kt
@@ -0,0 +1,7 @@
+package ch.keepcalm.kotlin
+
+class Constants(){
+    companion object {
+        const val RED = "Red"
+    }
+}
---
+++ src/test/kotlin/ch/keepcalm/kotlin/Main.kt
@@ -0,0 +1,8 @@
+package ch.keepcalm.kotlin
+
+import ch.keepcalm.kotlin.Constants.Companion.RED
+
+fun main(args: Array<String>) {
+    println("the color is $RED")
+
+}
