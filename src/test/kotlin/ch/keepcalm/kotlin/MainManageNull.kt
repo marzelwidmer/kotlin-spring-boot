b55a0bd5fb44b742615a4cd4c07b40f6e95db034
Marcel Widmer <marzelwidmer@gmail.com>
2018-03-30 13:27:31 +0200

    ManageNulpointers

---
+++ src/test/kotlin/ch/keepcalm/kotlin/MainManageNull.kt
@@ -0,0 +1,36 @@
+package ch.keepcalm.kotlin
+
+fun main(args: Array<String>) {
+    var nullValue:String? = null
+    println(nullValue)
+
+
+    val l:Int? = nullValue?.length
+    println("Length is $l")
+
+    // Java style
+    if(l == null){
+        println("Java style length is null")
+    }
+
+    val message = if(l != null){
+        "Lenght: $l"
+    } else {
+        "l is null"
+    }
+    println(message)
+
+    val l2 = l ?: -1
+    println("The value of l2 is $l2")
+
+
+    // kotlin.KotlinNullPointerException
+    try {
+        val l3 = l!!
+        println("The value of l3 is $l3")
+    }catch (e: KotlinNullPointerException){
+        println("l3 is null")
+    }
+
+
+}
