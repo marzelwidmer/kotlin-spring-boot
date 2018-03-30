c262877f7055f0de2145c05293a8c13983a07b43
Marcel Widmer <marzelwidmer@gmail.com>
2018-03-30 08:41:34 +0200

    add MyKotline playground class

---
+++ src/test/kotlin/ch/keepcalm/kotlin/MyKotline.kt
@@ -0,0 +1,16 @@
+package ch.keepcalm.kotlin
+
+val name = "Marcel"
+
+fun main(args: Array<String>) {
+
+    var num1 = 45.0
+    println("The value of num1 is $num1")
+    num1++
+    println("The new value of num1 is $num1")
+    println("Hello $name")
+
+    val num2:Double = 5.toDouble()
+    println("The value of num2 is $num2")
+
+}
