a7e40dbb8b71abb0f4acfcbc759c6d0dbcc6b17f
Marcel Widmer <marzelwidmer@gmail.com>
2018-03-31 12:12:34 +0200

    change price from val to var so change the price on runtime

--- src/test/kotlin/ch/keepcalm/kotlin/dataclass/ClothingItem.kt
+++ src/test/kotlin/ch/keepcalm/kotlin/dataclass/ClothingItem.kt
@@ -2,5 +2,5 @@ package ch.keepcalm.kotlin.dataclass
 
 data class ClothingItem(val type: String,
                         val size: String,
-                        val price: Double) {
+                        var price: Double) {
 }
--- src/test/kotlin/ch/keepcalm/kotlin/dataclass/Main.kt
+++ src/test/kotlin/ch/keepcalm/kotlin/dataclass/Main.kt
@@ -4,4 +4,7 @@ fun main(args: Array<String>) {
 
     val clothingItem = ClothingItem("Shirt", "L", 19.95)
     println(clothingItem)
+
+    clothingItem.price = 10.45
+    println(clothingItem)
 }
