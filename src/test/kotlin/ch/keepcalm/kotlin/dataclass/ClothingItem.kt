8168237ac985bc445469d16749238e5d52f71c01
Marcel Widmer <marzelwidmer@gmail.com>
2018-03-31 12:10:25 +0200

    Data class

---
+++ src/test/kotlin/ch/keepcalm/kotlin/dataclass/ClothingItem.kt
@@ -0,0 +1,6 @@
+package ch.keepcalm.kotlin.dataclass
+
+data class ClothingItem(val type: String,
+                        val size: String,
+                        val price: Double) {
+}
---
+++ src/test/kotlin/ch/keepcalm/kotlin/dataclass/Main.kt
@@ -0,0 +1,7 @@
+package ch.keepcalm.kotlin.dataclass
+
+fun main(args: Array<String>) {
+
+    val clothingItem = ClothingItem("Shirt", "L", 19.95)
+    println(clothingItem)
+}
