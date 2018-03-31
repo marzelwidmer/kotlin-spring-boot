6ad0d57cefa04aa789ca784342fbc286c5fb3c67
Marcel Widmer <marzelwidmer@gmail.com>
2018-03-31 17:29:51 +0200

    work also with Java

--- src/test/kotlin/ch/keepcalm/kotlin/dataclass/Main.kt
+++ src/test/kotlin/ch/keepcalm/kotlin/dataclass/Main.kt
@@ -1,5 +1,7 @@
 package ch.keepcalm.kotlin.dataclass
 
+import java.text.NumberFormat
+
 fun main(args: Array<String>) {
 
     val clothingItem = ClothingItem("Shirt", "L", 19.95)
@@ -13,8 +15,15 @@ fun main(args: Array<String>) {
     println("Item type = ${clothingItem2.type}")
 
     clothingItem2.price = 10.0
-    println("ClothingItme 2 price is included 10% discount : ${clothingItem2.price}")
+
+    //Locale.setDefault(Locale("de", "CH"))
+    val formatter = NumberFormat.getCurrencyInstance()
+    println("ClothingItem 2 price is included 10% discount : " +
+            "${formatter.format(clothingItem2.price)}")
 
     val person = Person("Marcel", "Widmer")
     println("That person is ${person.fullName}")
+
+
+
 }
