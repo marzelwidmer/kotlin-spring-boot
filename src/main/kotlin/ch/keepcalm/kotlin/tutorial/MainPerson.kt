f76655f9ad613dc98ccaf50d62c1c3c89aec39be
Marcel Widmer <marzelwidmer@gmail.com>
2018-04-02 08:53:55 +0200

    classes

---
+++ src/main/kotlin/ch/keepcalm/kotlin/tutorial/MainPerson.kt
@@ -0,0 +1,6 @@
+package ch.keepcalm.kotlin.tutorial
+
+fun main(args: Array<String>) {
+    var person= Person("Marcel", "Widmer")
+
+}
---
+++ src/main/kotlin/ch/keepcalm/kotlin/tutorial/Person.kt
@@ -0,0 +1,8 @@
+package ch.keepcalm.kotlin.tutorial
+
+class Person (firstName: String, lastName: String){
+
+    init {
+        println("Creating a person named $firstName $lastName")
+    }
+}
