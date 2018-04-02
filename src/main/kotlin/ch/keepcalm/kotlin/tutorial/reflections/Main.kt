64ad9624bb020f233c4bd16950a2139e9eaeefb9
Marcel Widmer <marzelwidmer@gmail.com>
2018-04-02 09:57:18 +0200

    reflections

--- src/main/kotlin/ch/keepcalm/kotlin/tutorial/reflections/Main.kt
+++ src/main/kotlin/ch/keepcalm/kotlin/tutorial/reflections/Main.kt
@@ -1,11 +1,20 @@
 package ch.keepcalm.kotlin.tutorial
 
+import kotlin.reflect.full.memberProperties
+
 fun main(args: Array<String>) {
     val me = Person("Marcel", "Widmer")
-    val kClass = me.javaClass.kotlin
-    println("Simple name = ${kClass.simpleName}")
-    println("Simple name = ${kClass.qualifiedName}")
-
     val basco = Dog("Terrier")
     println("My dog is a ${basco.breed}")
+
+
+    val kClass = basco.javaClass.kotlin
+    println("Simple name = ${kClass.simpleName}")
+    println("Qualified name = ${kClass.qualifiedName}")
+
+    for (name in kClass.memberProperties) {
+        println("Property name= $name")
+    }
+
+
 }
