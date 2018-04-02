2f32dee82de92c80edf8e76846e4ac9a94f3a038
Marcel Widmer <marzelwidmer@gmail.com>
2018-04-02 16:49:30 +0200

    sequences an lambda

--- src/main/kotlin/ch/keepcalm/kotlin/tutorial/sequences/Sequences.kt
+++ src/main/kotlin/ch/keepcalm/kotlin/tutorial/sequences/Sequences.kt
@@ -2,13 +2,17 @@ package ch.keepcalm.kotlin.tutorial.sequences
 
 import ch.keepcalm.kotlin.tutorial.lambda.getStudents
 
-fun sequences(){
+fun sequences() {
     val students = getStudents()
     val sqStudents = students.drop(1).take(3).toList()
     println("Sequenced Students: ${sqStudents}")
 
+    val numbers = generateSequence(100) { it + 1 }
+    println("Numbers: " + numbers.drop(5).take(10).toList())
 
-
+    val squares = generateSequence(1) { it + 1 }.map { it * it }
+    val evenSquares = squares.filter { it % 2 == 0 }
+    println("Even Squares: " + evenSquares.take(5).toList())
 }
 
 
