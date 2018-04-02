0bdca7ff24a0314e7a792d7f370422551d3f5fb7
Marcel Widmer <marzelwidmer@gmail.com>
2018-04-02 09:20:27 +0200

    generics

---
+++ src/main/kotlin/ch/keepcalm/kotlin/tutorial/generics/Main.kt
@@ -0,0 +1,14 @@
+package ch.keepcalm.kotlin.tutorial.generics
+
+fun main(args: Array<String>) {
+
+    val maxInt: Int = max(42, 99)
+    val maxLong: Long = max(123456789L, 999999999999L)
+    val maxByte: Byte = max((-128).toByte(), (127).toByte())
+    val maxString: String= max("Beta", "Alpha")
+
+    println("The max Int = $maxInt")
+    println("The max Long = $maxLong")
+    println("The max Byte = $maxByte")
+    println("The max String = $maxString")
+}
---
+++ src/main/kotlin/ch/keepcalm/kotlin/tutorial/generics/Max.kt
@@ -0,0 +1,6 @@
+package ch.keepcalm.kotlin.tutorial.generics
+
+fun <T: Comparable<T>> max(param1: T, param2: T): T {
+    val result = param1.compareTo(param2)
+    return if (result > 0) param1 else param2
+}
