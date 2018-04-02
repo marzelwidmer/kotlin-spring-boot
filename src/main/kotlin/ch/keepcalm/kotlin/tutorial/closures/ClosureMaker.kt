5a64761b23bf7cdd1ec95090cae6528924ec6424
Marcel Widmer <marzelwidmer@gmail.com>
2018-04-02 13:28:43 +0200

    A closure is when the local variables of a function are kept alive after the function has ended.

---
+++ src/main/kotlin/ch/keepcalm/kotlin/tutorial/closures/ClosureMaker.kt
@@ -0,0 +1,21 @@
+package ch.keepcalm.kotlin.tutorial.closures
+
+fun closureMaker(): () -> Unit {
+    var num = 0
+    return { println(num++) }
+}
+
+fun main(args: Array<String>) {
+
+    val myCounter1 = closureMaker()
+    val myCounter2 = closureMaker()
+    myCounter1()
+    myCounter1()
+    myCounter1()
+    myCounter1()
+    myCounter2()
+    myCounter2()
+    myCounter1()
+
+
+}
