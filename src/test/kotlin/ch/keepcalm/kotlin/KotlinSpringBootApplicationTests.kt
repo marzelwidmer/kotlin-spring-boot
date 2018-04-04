ee7afee43aa4216f6090787b7fd0f331d408b468
Marcel Widmer <marzelwidmer@gmail.com>
2018-04-04 09:35:59 +0200

    add Rest service test with kotlin

--- src/test/kotlin/ch/keepcalm/kotlin/KotlinSpringBootApplicationTests.kt
+++ src/test/kotlin/ch/keepcalm/kotlin/KotlinSpringBootApplicationTests.kt
@@ -1,10 +1,36 @@
 package ch.keepcalm.kotlin
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.junit4.SpringRunner
 
-//@RunWith(SpringRunner::class)
-//@SpringBootTest
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(classes = arrayOf(KotlinSpringBootApplication::class),
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class KotlinSpringBootApplicationTests {
+
+	@Autowired
+	lateinit var testRestTemplate: TestRestTemplate
+
+	@Test
+	fun whenCalledShouldReturnHello() {
+		val result = testRestTemplate
+				// ...
+				.getForEntity("/greeting", String::class.java)
+
+		assertNotNull(result)
+		assertEquals(result?.statusCode, HttpStatus.OK)
+		val resultExpected = "{\"id\":1,\"content\":\"Hello, World\"}"
+		assertEquals(result?.body, resultExpected)
+	}
+
 
 	@Test
 	fun contextLoads() {
